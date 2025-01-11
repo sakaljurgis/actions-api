9a87526982fca5519506c30f1dcbf1d700e30e9c
Jurgis Sakalauskas <jurgis.sakalauskas@cec.valantic.com>
2025-01-11 11:33:09 +0200

    add phpinfo

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/generate-image', [GoogleImageGenerationController::class, 'generateImage']);
 Route::post('/generate-image', [GoogleImageGenerationController::class, 'generateImage']);
+
+Route::get('/phpinfo', function() {
+    return phpinfo();
+});
